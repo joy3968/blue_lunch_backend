2ba2c5fc6d250fb4b07baee5530220ea7c49b4d3
ICT-GONG-MIN-WO\ICT-공민우 <gmw@bluemtec.com>
2024-12-02 13:43:13 +0900

    Blue Lunch Backend 초기 세팅

---
+++ src/main/java/com/bluedino/blue_lunch_backend/BlueLunchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bluedino.blue_lunch_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueLunchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueLunchBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bluedino/blue_lunch_backend/BlueLunchBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluedino.blue_lunch_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueLunchBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
