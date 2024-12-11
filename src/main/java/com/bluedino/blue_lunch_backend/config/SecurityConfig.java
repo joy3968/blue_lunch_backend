f07a12dc1eb1e884cee95916666c37e12f448253
MinWoo <sayo1z2@naver.com>
2024-12-11 22:49:42 +0900

    인가 없이 허용되는 경로, 테스트 경로 제거

--- src/main/java/com/bluedino/blue_lunch_backend/config/SecurityConfig.java
+++ src/main/java/com/bluedino/blue_lunch_backend/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/login", "/", "/join", "/api/data").permitAll()
+                        .requestMatchers("/login", "/", "/join").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated());
 
